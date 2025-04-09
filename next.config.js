0f883c3a4a93e52d2c823be325875bd9cdc49bd0
vashu <mandoravashu57@gmail.com>
2025-04-09 13:20:09 +0530

    update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,29 @@
-module.exports = {
-  output: 'export', // Enables static export
-  trailingSlash: true, // Ensures proper static routing
+// module.exports = {
+//   output: 'export', // Enables static export
+//   trailingSlash: true, // Ensures proper static routing
+//   images: {
+//     unoptimized: true, // Disables Next.js image optimization
+//   },
+// };
+import 'dotenv/config';
+
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
   images: {
-    unoptimized: true, // Disables Next.js image optimization
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'example.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'another-example.com',
+        pathname: '/images/**',
+      },
+    ],
   },
 };
+
+export default nextConfig;
