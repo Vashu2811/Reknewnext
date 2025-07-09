2d4117b776c88e5f6175340019d9fe62ec26ebab
vashu <mandoravashu57@gmail.com>
2025-07-09 12:33:50 +0530

    #build  error  solve

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -23,11 +23,36 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${nunito.variable} font-sans`} suppressHydrationWarning>
-       
+        <ThemeProvider>
+          <Navbar />
+          <ScrollToBottomButton />
+          <>
+            <div className="fixed inset-0 pointer-events-none z-0">
+              <Image
+                src={color}
+                alt="color-sharp"
+                className="absolute w-[200px] top-36 md:top-60 right-0 lg:w-[350px] lg:top-28 lg:right-56"
+                style={{ zIndex: 1 }}
+                loading="lazy"
+              />
+            </div>
+            <div className="fixed inset-0 pointer-events-none z-0">
+              <div
+                className="absolute bottom-1 right-[10%] w-64 h-64 bg-gradient-to-br from-[#FF512F]/5 to-[#FF512F]/5 dark:from-[#FF512F]/5 dark:to-[#FF512F]/5 
+                rounded-full blur-3xl"
+              />
+              <div
+                className="absolute bottom-[30%] left-[5%] w-64 h-64 bg-gradient-to-br from-[#FF512F]/5 to-[#FF512F]/5 dark:from-[#FF512F]/5 dark:to-[#FF512F]/5 
+                rounded-full blur-3xl"
+              />
+            </div>
+          </>
           <main className="min-h-screen transition-colors duration-300 overflow-x-hidden">
             {children}
           </main>
-         
+          <ScrollToTopButton />
+          <Footer />
+        </ThemeProvider>
       </body>
     </html>
   );
